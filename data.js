b466f17f64fd9d63449927c43c31c02816b7558f
Evgeniya <112981213+jenyadev3@users.noreply.github.com>
2023-04-30 14:43:15 +0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,81 @@
+const words = [
+    {
+      id: '1',
+      token: 'fox',
+      word: 'Der Fuchs',
+      type: 'word'
+    },
+    {
+      id: '2',
+      token: 'fox',
+      url: 'images/photo-1.jpg',
+      type: 'image'
+    },
+    {
+      id: '3',
+      token: 'turtle',
+      word: 'Die Schildkröte',
+      type: 'word'
+    },
+    {
+      id: '4',
+      token: 'turtle',
+      url: 'images/photo-2.jpg',
+      type: 'image'
+    },
+    {
+      id: '5',
+      token: 'elephant',
+      word: 'Der Elefant',
+      type: 'word'
+    },
+    {
+      id: '6',
+      token: 'elephant',
+      url: 'images/photo-3.jpg',
+      type: 'image'
+    },
+    {
+      id: '7',
+      token: 'squirrel',
+      word: 'Das Eichhorn',
+      type: 'word'
+    },
+    {
+      id: '8',
+      token: 'squirrel',
+      url: 'images/photo-4.jpg',
+      type: 'image'
+    },
+    {
+      id: '9',
+      token: 'raccoon',
+      word: 'Der Waschbär',
+      type: 'word'
+    },
+    {
+      id: '10',
+      token: 'raccoon',
+      url: 'images/photo-5.jpg',
+      type: 'image'
+    },
+    {
+      id: '11',
+      token: 'deer',
+      word: 'Der Hirsch',
+      type: 'word'
+    },
+    {
+      id: '12',
+      token: 'deer',
+      url: 'images/photo-6.jpg',
+      type: 'image'
+    }
+  ];
+
+  const selectedItems = ['5', '4'];
+  const finishedItems = ['8', '12'];
+  
+  window.words = words;
+  window.selectedItems = selectedItems;
+  window.finishedItems = finishedItems;
