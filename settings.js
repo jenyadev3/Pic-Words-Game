1d545f6b42a4fd1e7be25e037432734da158b521
jenyadev3 <112981213+jenyadev3@users.noreply.github.com>
2023-05-09 14:42:22 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,5 +1,11 @@
 const TIMEOUT = 800;
 const LIVES_COUNT = 3;
 
+const AppRoute = {
+    Game: 'game',
+    Results: 'results',
+}
+
 window.TIMEOUT = TIMEOUT;
 window.LIVES_COUNT = LIVES_COUNT;
+window.AppRoute = AppRoute;
