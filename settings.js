091aacafde2eec399bf0d1eb076883b43a57489f
jenyadev3 <112981213+jenyadev3@users.noreply.github.com>
2023-05-22 19:41:08 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -6,6 +6,16 @@ const AppRoute = {
     Results: 'results',
 }
 
+// В разметке экрана есть три одинаковых элемента кнопки, которые отличаются подписями и иконками. 
+// К каждой кнопке привязан свой набор слов для игры.
+
+const GAME_TYPES = [
+    { type: 'finnish', icon: 'fin', label: 'aloittaa' },
+    { type: 'german', icon: 'ger', label: 'beginnen' },
+    { type: 'italian', icon: 'ita', label: 'iniziare' },
+  ];
+  
+
 window.TIMEOUT = TIMEOUT;
 window.LIVES_COUNT = LIVES_COUNT;
 window.AppRoute = AppRoute;
